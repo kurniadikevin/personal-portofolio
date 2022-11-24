60fdab12244a5e09ce79a609170f47c6d1e06a2d
Kevin Kurniadi <kurniadikevin@gmail.com>
2022-11-24 16:58:58 +0700

    update project screenshot and links

---
+++ script.js
@@ -0,0 +1,11 @@
+//hover caption for stack icon
+const icon= document.querySelectorAll('#stack-icon');
+const iconCaption = document.querySelectorAll('.caption');
+for(let i=0; i< icon.length; i++){
+    icon[i].addEventListener('mouseover',()=>{
+        iconCaption[i].style.display='inline';
+    })
+    icon[i].addEventListener('mouseleave',()=>{
+        iconCaption[i].style.display='none';
+    })
+}
